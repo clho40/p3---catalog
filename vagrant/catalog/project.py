ba20d1b1215420dfe6069693673394316bddd72e
clho40 <clho40@hotmail.com>
2015-09-04 00:13:58 +0800

    update 5

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -12,7 +12,7 @@ def newCatagory():
     catagories = database_service.GetAllCatagory()
     if request.method == 'POST':
         database_service.NewCatagory(request.form['name'],request.form['desc'])
-        flash('New catagory created!')
+        flash('New catagory created!','alert-success')
         return redirect(url_for('newCatagory',catagories=catagories))
     else:
         return render_template('newcatagory.html',catagories=catagories)
@@ -22,18 +22,18 @@ def editCatagory(cid):
     catagories = database_service.GetAllCatagory()
     if request.method == 'POST':
         database_service.EditCatagory(cid,request.form['name'],request.form['desc'])
-        flash('Catagory updated!')
+        flash('Catagory updated!','alert-success')
         return redirect(url_for('showProducts',cid=cid))
     else:
         sel_catagory = database_service.GetCatagoryByID(cid)
         return render_template('editcatagory.html',catagories=catagories,sel_catagory=sel_catagory)
 
-@app.route('/catagory/<int:cid>/delete')
+@app.route('/catagory/<int:cid>/delete', methods=['GET','POST'])
 def deleteCatagory(cid):
     catagories = database_service.GetAllCatagory()
     if request.method == 'POST':
         database_service.DeleteCatagory(cid)
-        flash('Catagory deleted!')
+        flash('Catagory deleted!','alert-success')
         return redirect(url_for('IndexPage'))
     else:
         sel_catagory = database_service.GetCatagoryByID(cid)
@@ -45,6 +45,45 @@ def showProducts(cid):
     sel_catagory = database_service.GetCatagoryByID(cid)
     products = database_service.GetProductByCatagory(cid)
     return render_template('products.html',catagories=catagories,sel_catagory=sel_catagory, products=products)
+
+@app.route('/product/new', methods=['GET','POST'])
+def newProduct():
+    catagories = database_service.GetAllCatagory()
+    if request.method == 'POST':
+        database_service.NewProduct(request.form['name'],request.form['desc'],request.form['price'],request.form['flavour'],request.form['catagory'])
+        flash('New product created!','alert-success')
+        return redirect(url_for('newProduct',catagories=catagories))
+    else:
+        return render_template('newproduct.html',catagories=catagories)
+
+@app.route('/catagory/<int:cid>/product/<int:pid>/edit', methods=['GET','POST'])
+def editProduct(cid,pid):
+    catagories = database_service.GetAllCatagory()
+    if request.method == 'POST':
+        database_service.EditProduct(pid,request.form['name'],request.form['desc'],request.form['price'],request.form['flavour'],request.form['catagory'])
+        flash('Product updated!','alert-success')
+        return redirect(url_for('showProducts',cid=cid))
+    else:
+        sel_catagory = database_service.GetCatagoryByID(cid)
+        sel_product = database_service.GetProductByID(pid)
+        return render_template('editproduct.html',catagories=catagories,sel_catagory=sel_catagory, sel_product=sel_product)
+
+@app.route('/catagory/<int:cid>/product/<int:pid>/delete', methods=['GET','POST'])
+def deleteProduct(cid,pid):
+    catagories = database_service.GetAllCatagory()
+    if request.method == 'POST':
+        database_service.DeleteProduct(pid)
+        flash('Product deleted!','alert-success')
+        return redirect(url_for('showProducts',cid=cid))
+    else:
+        sel_catagory = database_service.GetCatagoryByID(cid)
+        sel_product = database_service.GetProductByID(pid)
+        return render_template('deleteproduct.html',catagories=catagories,sel_catagory=sel_catagory,sel_product=sel_product)
+
+@app.route('/sidebar')
+def sidebar():
+    return render_template('sidebar.html')
+
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
